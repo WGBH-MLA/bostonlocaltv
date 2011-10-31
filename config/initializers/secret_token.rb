fdf7f54f02340e1dc5d2d1d2b5993130b1401b63
Chris Beer <chris_beer@wgbh.org>
2011-10-31 13:17:37 -0400

    rails new bostonlocaltv

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bostonlocaltv::Application.config.secret_token = '3738665f19a508ced528d28154b4f28b8c76e43b0128ec451f6aab1c122efa28df8a781c313edd6befcfc81f88d93ff56c194e2200ff00c7819267c5f496713b'
