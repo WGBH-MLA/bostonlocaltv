8f303e78180f07bd3ce035fe2c92fd6a177473b9
Mary Bennett <yuanwei2@hotmail.com>
2012-10-01 12:22:53 -0400

    added view

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,10 +1,10 @@
 module ItemsHelper
-  def get_vote_count (doc_id)                                    
+   def get_vote_count?(doc_id)
     begin
       @item = Item.find (doc_id)
       @item.votes_count
     rescue
-      @item.votes_count = ""
+      @item.votes_count = "" 
     end
-  end  
+  end
 end
