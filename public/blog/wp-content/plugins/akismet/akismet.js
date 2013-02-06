9af13912f7a6a942b43f5a5caf168a52a9c4de0e
Andrew Myers <andrew_myers@wgbh.org>
2013-02-06 12:25:29 -0500

    Upgraded wordpress to 3.5.1

--- public/blog/wp-content/plugins/akismet/akismet.js
+++ public/blog/wp-content/plugins/akismet/akismet.js
@@ -21,6 +21,7 @@ jQuery(document).ready(function () {
  		var thisId = jQuery(this).attr('commentid');
  		var data = {
  			action: 'comment_author_deurl',
+			_wpnonce: WPAkismet.comment_author_url_nonce,
  			id: thisId
  		};
  		jQuery.ajax({
@@ -48,6 +49,7 @@ jQuery(document).ready(function () {
 		var thisUrl = jQuery(this).attr('href').replace("http://www.", "").replace("http://", "");
  		var data = {
  			action: 'comment_author_reurl',
+			_wpnonce: WPAkismet.comment_author_url_nonce,
  			id: thisId,
  			url: thisUrl
  		};
@@ -72,6 +74,7 @@ jQuery(document).ready(function () {
  		return false;
  	});
  	jQuery('a[id^="author_comment_url"]').mouseover(function () {
+		var wpcomProtocol = ( 'https:' === location.protocol ) ? 'https://' : 'http://';
 		// Need to determine size of author column
 		var thisParentWidth = jQuery(this).parent().width();
 		// It changes based on if there is a gravatar present
@@ -81,12 +84,12 @@ jQuery(document).ready(function () {
 			jQuery('.widefat td').css('overflow', 'visible');
 			jQuery(this).css('position', 'relative');
 			var thisHref = jQuery.URLEncode(jQuery(this).attr('href'));
-			jQuery(this).append('<div class="mShot mshot-container" style="left: '+thisParentWidth+'"><div class="mshot-arrow"></div><img src="http://s.wordpress.com/mshots/v1/'+thisHref+'?w=450" width="450" class="mshot-image_'+thisId+'" style="margin: 0;" /></div>');
+			jQuery(this).append('<div class="mShot mshot-container" style="left: '+thisParentWidth+'"><div class="mshot-arrow"></div><img src="'+wpcomProtocol+'s0.wordpress.com/mshots/v1/'+thisHref+'?w=450" width="450" class="mshot-image_'+thisId+'" style="margin: 0;" /></div>');
 			setTimeout(function () {
-				jQuery('.mshot-image_'+thisId).attr('src', 'http://s.wordpress.com/mshots/v1/'+thisHref+'?w=450&r=2');
+				jQuery('.mshot-image_'+thisId).attr('src', wpcomProtocol+'s0.wordpress.com/mshots/v1/'+thisHref+'?w=450&r=2');
 			}, 6000);
 			setTimeout(function () {
-				jQuery('.mshot-image_'+thisId).attr('src', 'http://s.wordpress.com/mshots/v1/'+thisHref+'?w=450&r=3');
+				jQuery('.mshot-image_'+thisId).attr('src', wpcomProtocol+'s0.wordpress.com/mshots/v1/'+thisHref+'?w=450&r=3');
 			}, 12000);
 		} else {
 			jQuery(this).find('.mShot').css('left', thisParentWidth).show();
@@ -101,4 +104,11 @@ jQuery.extend({URLEncode:function(c){var o='';var x=0;c=c.toString();var r=/(^[a
     if(m!=null && m.length>1 && m[1]!=''){o+=m[1];x+=m[1].length;
     }else{if(c[x]==' ')o+='+';else{var d=c.charCodeAt(x);var h=d.toString(16);
     o+='%'+(h.length<2?'0':'')+h.toUpperCase();}x++;}}return o;}
+});
+// Preload mshot images after everything else has loaded
+jQuery(window).load(function() {
+	var wpcomProtocol = ( 'https:' === location.protocol ) ? 'https://' : 'http://';
+	jQuery('a[id^="author_comment_url"]').each(function () {
+		jQuery.get(wpcomProtocol+'s0.wordpress.com/mshots/v1/'+jQuery.URLEncode(jQuery(this).attr('href'))+'?w=450');
+	});
 });
