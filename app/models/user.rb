b101396b1f047bc7086bc4edc766f38b223b4c42
Joshua Wilcox <jwilcox09@gmail.com>
2013-09-20 09:41:02 -0400

    cleaned up some styles and the stylesheets

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,17 @@ class User < ActiveRecord::Base
   
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
+  
+  validates_presence_of :email, :on => :create, :message => "can't be blank"
+  validates_presence_of :password, :on => :create, :message => "can't be blank"
+  validates_presence_of :password_confirmation, :on => :create, :message => "can't be blank"
+  validates_presence_of :first_name, :on => :create, :message => "can't be blank"
+  validates_presence_of :last_name, :on => :create, :message => "can't be blank"
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 
   # Method added by Blacklight; Blacklight uses #to_s on your
   # user class to get a user-displayable login/identifier for
