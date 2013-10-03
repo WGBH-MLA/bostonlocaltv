913ac7ddc50f98e62369f7f4b5d881a28b39f8b5
Joshua Wilcox <jwilcox09@gmail.com>
2013-10-03 14:11:27 -0400

    fixing production assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,9 +15,10 @@ Bostonlocaltv::Application.configure do
   config.assets.compress = true
 
   config.assets.precompile = [ /\w+\.(?!js|css).+/, ]
+  config.assets.precompile += ['bostonlocaltv.css', 'active_admin.css', 'video-js.min.css']
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
