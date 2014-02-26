2c88bfa9f39f6a3d05db3a0dcc875b24bf6104c0
Renae Bair <renaebair@gmail.com>
2014-02-26 11:35:26 -0500

    test for notifing user via email of request approval

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,15 @@
 require "spec_helper"
 
 describe UserMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+	before do
+	  @user = create(:user)
+  	artifact = create(:artifact)
+  	artifact.request!(@user)
+  	artifact.save
+  	artifact.approve!
+  end
+
+  it "should email a user when a request is approved by an admin" do
+  	ActionMailer::Base.deliveries.last.to.should == [@user.email]
+  end
 end
