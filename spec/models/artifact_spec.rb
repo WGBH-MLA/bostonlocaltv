7cf312c2fc110ee45bb354f6589472139690539c
Renae Bair <renaebair@gmail.com>
2014-02-28 21:05:29 -0500

    Tests for artifact sponsorship feature & withdraw req functionality

--- spec/models/artifact_spec.rb
+++ spec/models/artifact_spec.rb
@@ -4,6 +4,7 @@ describe Artifact do
 
 	before do
 		@user = create(:user)
+		@user2 = create(:user)
 		@artifact = create(:artifact)
 	end
 
@@ -27,12 +28,46 @@ describe Artifact do
 		end
 
 		it "artifact saves state of requested after multiple users request digitization" do 
-			@user2 = create(:user)
 			@user3 = create(:user)
 			@artifact.request_digitization(@user)
 			@artifact.request_digitization(@user2)
 			@artifact.request_digitization(@user3)
 			@artifact.state.should == "requested"
+		end
+
+		it "should return artifact's state to initiated if last request is withdrawn" do 
+			@artifact.request_digitization(@user)
+			@artifact.request_digitization(@user2)
+			@artifact.withdraw_request(@user)
+			@artifact.withdraw_request(@user2)
+			@artifact.state.should == "initiated"
+		end
+
+		it "should keep artifact's state as 'requested' if a request is withdrawn but there are other remaining requests" do 
+			@artifact.request_digitization(@user)
+			@artifact.request_digitization(@user2)
+			@artifact.withdraw_request(@user)
+			@artifact.state.should == "requested"
+		end
+	end
+
+	describe "sponsors" do
+
+		it 'should make a user who requests an artifact a potential sponsor' do
+			@artifact.request_digitization(@user)
+			@artifact.potential_sponsors.should include(@user)
+		end
+
+		it 'should not make user a confirmed sponsor upon artifact request' do
+			@artifact.request_digitization(@user)
+			@artifact.sponsors.should_not include(@user)
+		end
+
+		it "should remove user's potential sponsorship if artifact request is withdrawn" do
+			@artifact.request_digitization(@user)
+			@artifact.withdraw_request(@user)
+			@artifact.reload
+			@artifact.potential_sponsors.should_not include(@user)
 		end
 
 	end
