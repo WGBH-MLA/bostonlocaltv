e30013c9e233ca02ab7b5bd12858f56a8be4452d
Renae Bair <renaebair@gmail.com>
2014-03-12 22:53:35 -0400

    WIP tests for artifact show logic
    These are not working at the moment - spent maybe too much time trying
    to figure out how to stub out the blacklight stuff so I could get to a
    point in the test where I could actually visit the catalog show path in
    order to test all the work I did in the previous commit, regarding the
    refactor of the logic related to the artifact/catalog show feature. I
    will need to connect with Drew on this tomorrow if we do want to move
    forward with tests for this, because I wasn't able to get it quite where
    I needed to.

---
+++ spec/features/catalog_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe 'Catalog Show Page', type: feature do
+	before(:each) do
+    @user = create(:user, :password => 'password', :password_confirmation => 'password')
+    visit new_user_session_path
+    fill_in 'Email', :with => @user.email
+    fill_in 'Password', :with => 'password'
+    click_button 'Sign in'
+    @artifact = create(:artifact)
+
+    YAML::ENGINE.yamler = 'syck'
+    solr_fixture_file = File.dirname(__FILE__) + "/../fixtures/solr_response.yml"
+    solr_response = YAML::load_file(solr_fixture_file)
+
+    solr_fixture_file_r = File.dirname(__FILE__) + "/../fixtures/solr_response_r.yml"
+    solr_response_r = YAML::load_file(solr_fixture_file_r)
+
+    solr_fixture_file_docs = File.dirname(__FILE__) + "/../fixtures/solr_response_docs.yml"
+    solr_response_docs = YAML::load_file(solr_fixture_file_docs)
+    
+    solr_response.stub(:response) {solr_response_r}
+    solr_response.stub(:docs) {solr_response_docs}
+    Blacklight::SolrResponse.stub(:new) {solr_response}
+  end
+
+  it "visits artifact catalog page" do
+    # puts @solr_response
+    # # visit "catalog/#{@artifact.solr_document_id}"
+    # visit 'catalog'
+    # save_and_open_page
+  end
+end
