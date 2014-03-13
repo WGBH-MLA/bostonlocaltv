4ae6b6808ac34d3f46c90373eb5d4c9a5074990f
Renae Bair <renaebair@gmail.com>
2014-03-13 11:29:40 -0400

    More specs for artifact helper

--- spec/features/catalog_spec.rb
+++ spec/features/catalog_spec.rb
@@ -1,14 +1,7 @@
 require 'spec_helper'
 
 describe 'Catalog Show Page', type: feature do
-	before(:each) do
-    @user = create(:user, :password => 'password', :password_confirmation => 'password')
-    visit new_user_session_path
-    fill_in 'Email', :with => @user.email
-    fill_in 'Password', :with => 'password'
-    click_button 'Sign in'
-    @artifact = create(:artifact)
-
+  before(:each) do
     YAML::ENGINE.yamler = 'syck'
     solr_fixture_file = File.dirname(__FILE__) + "/../fixtures/solr_response.yml"
     solr_response = YAML::load_file(solr_fixture_file)
@@ -18,14 +11,82 @@ describe 'Catalog Show Page', type: feature do
 
     solr_fixture_file_docs = File.dirname(__FILE__) + "/../fixtures/solr_response_docs.yml"
     solr_response_docs = YAML::load_file(solr_fixture_file_docs)
-    
+
     solr_response.stub(:response) {solr_response_r}
     solr_response.stub(:docs) {solr_response_docs}
     Blacklight::SolrResponse.stub(:new) {solr_response}
   end
 
-  it "visits artifact catalog page" do
+  it "tells user artifact cannot be digitized if artifact was blocked" do
+    @user = create(:user, :password => 'password', :password_confirmation => 'password')
+    visit new_user_session_path
+    fill_in 'Email', :with => @user.email
+    fill_in 'Password', :with => 'password'
+    click_button 'Sign in'
+
+    @artifact = create(:artifact)
+    @artifact.request_digitization(@user)
+    @sponsorship = @user.sponsorships.first
+    @artifact.block(@user)
     visit "catalog/#{@artifact.solr_document_id}"
-    save_and_open_page
+    expect(page).to have_content("We are unable to digitize this item.")
   end
+
+  it "tells the user they've already requested the artifact if they have an outstanding request on it" do
+    @user = create(:user, :password => 'password', :password_confirmation => 'password')
+    visit new_user_session_path
+    fill_in 'Email', :with => @user.email
+    fill_in 'Password', :with => 'password'
+    click_button 'Sign in'
+
+    @artifact = create(:artifact, solr_document_id: "V_GOMVH1RTSRIM6L6")
+    @artifact.request_digitization(@user)
+    @sponsorship = @user.sponsorships.first
+
+    visit "catalog/#{@artifact.solr_document_id}"
+    expect(page).to have_content("You have requested this item,")
+  end
+
+  # it "gives user option to track artifact if it's already being digitized" do
+  #   @user1 = create(:user, :password => 'password', :password_confirmation => 'password')
+
+  #   @artifact = create(:artifact, solr_document_id: "V_GOMVH1RTSRIM6L6")
+  #   @artifact.request_digitization(@user1)
+  #   # @artifact.approve_digitization(@user1)
+  #   @sponsorship = @user1.sponsorships.first
+
+  #   @user2 = create(:user, :password => 'password', :password_confirmation => 'password')
+  #   visit new_user_session_path
+  #   fill_in 'Email', :with => @user2.email
+  #   fill_in 'Password', :with => 'password'
+  #   click_button 'Sign in'
+
+  #   visit "catalog/#{@artifact.solr_document_id}"
+  #   expect(page).to have_content("Track")
+  # end
+
+  # it "displays 'request' option if artifact is not blocked and user has not requested it before" do 
+  #   @user3 = create(:user, :password => 'password', :password_confirmation => 'password')
+  #   visit new_user_session_path
+  #   fill_in 'Email', :with => @user2.email
+  #   fill_in 'Password', :with => 'password'
+  #   click_button 'Sign in'
+
+  #   @artifact = create(:artifact, solr_document_id: "V_GOMVH1RTSRIM6L6")
+  # end
+
+  # it "displays 'request' option if artifact is not blocked and user has not requested it before" do
+  #   @user2 = create(:user, :password => 'password', :password_confirmation => 'password')
+  #   visit new_user_session_path
+  #   fill_in 'Email', :with => @user2.email
+  #   fill_in 'Password', :with => 'password'
+  #   click_button 'Sign in'
+
+  #   @artifact = create(:artifact, solr_document_id: "V_GOMVH1RTSRIM6L6")
+  #   @artifact.request_digitization(@user1)
+  #   @sponsorship = @user1.sponsorships.first
+
+  #   visit "catalog/#{@artifact.solr_document_id}"
+  #   expect(page).to have_content("Request digitization of this item")
+  # end
 end
