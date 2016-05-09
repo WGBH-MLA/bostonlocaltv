a17e29be4e0f3334ba72b30aa2fc3e9917348d76
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-05-09 15:38:53 -0400

    Move schema back to the state at the very first check-in.

--- db/schema.rb
+++ db/schema.rb
@@ -9,28 +9,45 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended that you check this file into your version control system.
+# It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_141_106_192_110) do
-  create_table 'bookmarks', force: true do |t|
-    t.integer  'user_id', null: false
-    t.string   'user_type'
-    t.string   'document_id'
-    t.string   'title'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.string   'document_type'
+ActiveRecord::Schema.define(:version => 20111031172152) do
+
+  create_table "bookmarks", :force => true do |t|
+    t.integer  "user_id",     :null => false
+    t.string   "document_id"
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "user_type"
   end
 
-  add_index 'bookmarks', ['user_id'], name: 'index_bookmarks_on_user_id'
-
-  create_table 'searches', force: true do |t|
-    t.text     'query_params'
-    t.integer  'user_id'
-    t.string   'user_type'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
+  create_table "searches", :force => true do |t|
+    t.text     "query_params"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "user_type"
   end
 
-  add_index 'searches', ['user_id'], name: 'index_searches_on_user_id'
+  add_index "searches", ["user_id"], :name => "index_searches_on_user_id"
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                                 :default => "", :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                         :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
 end
