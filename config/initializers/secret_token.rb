eef0548f1bb826c4b9f1f0a55a43575b0a8a7879
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-05-12 15:39:39 -0400

    Random token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,8 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Bostonlocaltv::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
-  ('x' * 30)
-else
-  ENV['SECRET_TOKEN']
-end
+Bostonlocaltv::Application.config.secret_token = rand.to_s
+# We aren't using sessions, so this is mostly moot.
