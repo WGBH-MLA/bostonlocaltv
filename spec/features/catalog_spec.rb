d3bbe8256a0b7127c2c530f68e2371a5aaa437e4
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-05-12 12:36:12 -0400

    Load fixtures. Needs work.

--- spec/features/catalog_spec.rb
+++ spec/features/catalog_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe 'Catalog Show Page', type: feature do
+  before(:all) do
+    # TODO: This is just a copy and paste from .travis.yml. Should not rely on back-ticks
+    `for ORG in cctv wcvb wgbh whdh; do bundle exec rake data:$ORG file=spec/fixtures/xml/$ORG.pbcore.xml; done`
+  end
   it 'has a working #index' do
     visit 'catalog?non_video=yes&search_field=all_fields'
     expect(page).to have_content('Collection: WHDH')
