93a2a897f3fca5ffa903a65371681f3b104d2ee1
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-05-12 15:52:27 -0400

    Happy deploy?

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,5 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Bostonlocaltv::Application.config.secret_token = rand.to_s
-# We aren't using sessions, so this is mostly moot.
+Bostonlocaltv::Application.config.secret_token = "#{rand}#{rand}#{rand}#{rand}"
+# We aren't using sessions, so this is mostly moot. 30 character minimum.
