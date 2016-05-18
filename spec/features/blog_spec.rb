a867d0a7ed660e4cf44efe88e8ddfd3919519e96
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-05-18 15:06:47 -0400

    and test it. Fix #233.

--- spec/features/blog_spec.rb
+++ spec/features/blog_spec.rb
@@ -5,4 +5,13 @@ describe 'Blog Show Page', type: feature do
     visit "blog/about"
     expect(page).to have_content('Welcome to the Boston TV News Digital Library')
   end
+  it 'lists old blog pages' do
+    visit 'blog'
+    expect(page).to have_content('Boston Local TV News Project Launched!') # first
+    expect(page).to have_content('New Videos Up from Digitize on Demand Program') # last
+  end
+  it 'does not list non-blog pages' do
+    visit 'blog'
+    expect(page).not_to have_content('Resources for Archivists') # only on home
+  end
 end
