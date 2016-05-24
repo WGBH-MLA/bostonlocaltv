7c8244912d608be602650f46e2ea0a1b2748b568
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-05-24 14:34:24 -0400

    Check that we are producing the expected solr hash.

--- spec/models/xml_spec.rb
+++ spec/models/xml_spec.rb
@@ -1,5 +1,6 @@
 require 'active_support/core_ext/string'
 require 'active_model'
+require 'json'
 
 require_relative '../../app/models/dataset'
 require_relative '../../app/models/dataset/xml'
@@ -12,7 +13,14 @@ describe 'Xml (pbcore to solr conversion)' do
     it "translates #{File.basename(pbcore_path)} to #{File.basename(solr_path)}" do
       class_path = "#{__dir__}/../../app/models/dataset/#{station}"
       require class_path
-      "Dataset::#{station.titlecase}".constantize.new(pbcore_path)
+      dataset = "Dataset::#{station.titlecase}".constantize.new(pbcore_path)
+      records = dataset.records
+      expect(records.count).to eq(1)
+      solr_doc = {}
+      dataset.send(:process_record, records.first, solr_doc)
+      actual = JSON.pretty_generate(solr_doc)
+      expected = File.read(solr_path)
+      expect(actual).to eq(expected)
     end
   end
 end
