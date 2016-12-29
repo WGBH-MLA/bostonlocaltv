4829175e04b55ed0a5f76791e762c136aeeac19e
Jason Corum <jason.corum@gmail.com>
2016-12-29 15:16:30 -0500

    Sets up link ignore file and adds link check to blog test

--- spec/features/blog_spec.rb
+++ spec/features/blog_spec.rb
@@ -1,3 +1,4 @@
+require_relative '../support/validation_helper'
 require 'spec_helper'
 
 describe 'Blog Show Page', type: feature do
@@ -13,5 +14,14 @@ describe 'Blog Show Page', type: feature do
   it 'does not list non-blog pages' do
     visit 'blog'
     expect(page).not_to have_content('Resources for Archivists') # only on home
+  end
+
+  Blog.all.each do |blog|
+    path = "/blog/#{blog.path}"
+    it "#{path} works" do
+      visit path
+      expect(page.status_code).to eq(200)
+      expect_fuzzy_xml
+    end
   end
 end
