36280d1f16f05d1c3fd1b2e61218f9c4365364c7
Your Name <sadie_roosa@wgbh.org>
2019-05-02 12:04:49 -0400

    add names to MODS

--- app/controllers/oai_controller.rb
+++ app/controllers/oai_controller.rb
@@ -1,5 +1,5 @@
 class OaiController < ApplicationController
-  Record = Struct.new(:id, :date, :title, :descriptions, :subjects, :video, :locations, :date_created)
+  Record = Struct.new(:id, :date, :title, :descriptions, :subjects, :video, :locations, :date_created, :names)
   ROWS = 100
 
   def index
@@ -26,7 +26,7 @@ class OaiController < ApplicationController
              'rows' => ROWS,
              'start' => start
            })['response']['docs'].map do |d|
-        Record.new(d['id'], d['timestamp'], d['title_s'].first, d['description_s'], d['subject_s'], d['video_b'], d['location_s'], d['date_created_s'])
+        Record.new(d['id'], d['timestamp'], d['title_s'].first, d['description_s'], d['subject_s'], d['video_b'], d['location_s'], d['date_created_s'], d['name_s'])
       end
 
     # Not ideal: they'll need to go past the end.
